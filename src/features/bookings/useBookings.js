fa23fae51f893448298973e1b2e9cd96d80f0624
Karthic <jkarthickeyan24@gmail.com>
2025-02-11 18:57:34 +0530

    enhance useBookings to support pagination pre-fetching for improved performance

--- src/features/bookings/useBookings.js
+++ src/features/bookings/useBookings.js
@@ -1,22 +1,28 @@
-import { useQuery } from '@tanstack/react-query';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
 import { getBookingsApi } from '../../services/apiBookings';
 import { useSearchParams } from 'react-router-dom';
+import { RESULTS_PER_PAGE } from '../../utils/constant';
 
 export default function useBookings() {
+  const queryClient = useQueryClient();
   const [searchParams] = useSearchParams();
-  //FILTER
+
+  // FILTER
   const filterValue = searchParams.get('status');
   const filter =
     !filterValue || filterValue === 'all'
       ? null
       : { field: 'status', value: filterValue };
-  //SORT
+
+  // SORT
   const sortValue = searchParams.get('sort') || 'startDate-desc';
   const [field, direction] = sortValue.split('-');
   const sort = { field, direction };
-  //PAGINATION
+
+  // PAGINATION
   const page = !searchParams.get('page') ? 1 : Number(searchParams.get('page'));
 
+  // QUERY
   const {
     isPending,
     error,
@@ -25,6 +31,23 @@ export default function useBookings() {
     queryKey: ['bookings', filter, sort, page],
     queryFn: () => getBookingsApi({ filter, sort, page }),
   });
+
+  // PRE_FETCHING
+  const pageCount = Math.ceil(count / RESULTS_PER_PAGE);
+
+  if (page < pageCount) {
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sort, page + 1],
+      queryFn: () => getBookingsApi({ filter, sort, page: page + 1 }),
+    });
+  }
+
+  if (page > 1) {
+    queryClient.prefetchQuery({
+      queryKey: ['bookings', filter, sort, page - 1],
+      queryFn: () => getBookingsApi({ filter, sort, page: page - 1 }),
+    });
+  }
 
   return { isPending, error, bookings, count };
 }
